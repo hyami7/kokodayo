4d9b744fecc9505db929344ca8d3ceb4384222be
yangwenjian <yangwenjian@sikulifang.com>
2023-05-24 11:11:12 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ set -e
 rm -rf docs/.vitepress/dist/
 
 # 生成静态文件
-# pnpm build
+pnpm build
 
 # 进入生成的文件夹
-# cd docs/.vitepress/dist
+cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
