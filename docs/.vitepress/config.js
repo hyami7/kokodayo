b0e9157271a623bf70ae66f53070c1ba496eaa30
yangwenjian <yangwenjian@sikulifang.com>
2023-05-24 10:37:22 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "kokodayo",
+  base: '/kokodayo/',
+  description: "个人博客",
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '开发文档', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: '开发文档',
+        items: [
+          { text: '示例1', link: '/markdown-examples' },
+          { text: '示例2', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: page
+
+# asdadadsa
+
+hero:
+  name: kokodayo
+  text: 内容信息
+  tagline: 副内容信息
+  actions:
+    - theme: brand
+      text: 开发文档
+      link: /markdown-examples
+    - theme: alt
+      text: 示例
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
